cf7b0e06424fc7ef223bdf144ed2a6937f8a772c
Happy1353 <79153940348@yandex.ru>
2023-01-09 15:43:30 +0100

    init

--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,42 @@
-import { IExecutor } from './Executor';
-import ITask from './Task';
+import { IExecutor } from "./Executor";
+import ITask from "./Task";
 
-export default async function run(executor: IExecutor, queue: AsyncIterable<ITask>, maxThreads = 0) {
-    maxThreads = Math.max(0, maxThreads);
-    /**
-     * Код надо писать сюда
-     * Тут что-то вызываем в правильном порядке executor.executeTask для тасков из очереди queue
-     */
+export default async function run(
+  executor: IExecutor,
+  queue: AsyncIterable<ITask>,
+  maxThreads = 0
+): Promise<any> {
+  maxThreads = Math.max(0, maxThreads);
+  executor.start();
+
+  let runningPromises: Promise<void>[] = [];
+  const currentQueue = [];
+
+  for await (const i of queue) currentQueue.push(i);
+
+  while (currentQueue.length > 0) {
+    let qIdx = 0;
+    while (qIdx < currentQueue.length) {
+      const task = currentQueue[qIdx];
+
+      if (maxThreads > 0 && runningPromises.length >= maxThreads) {
+        break;
+      }
+
+      if (executor.executeData.running[task.targetId]) {
+        qIdx++;
+      } else {
+        currentQueue.splice(qIdx, 1);
+        const promise = executor.executeTask(task).then(() => {
+          runningPromises = runningPromises.filter((p) => p !== promise);
+        });
+        runningPromises.push(promise);
+      }
+    }
+    await Promise.race(runningPromises);
+  }
+
+  await Promise.all(runningPromises);
+
+  executor.stop();
 }
